ee3b8bb4e0cc3eb7211c5f4e420d2c12855f3177
mrmt <44957177+mrmt1204@users.noreply.github.com>
2019-04-22 00:54:52 +0900

    Update main.java

--- main.java
+++ main.java
@@ -1,124 +1,100 @@
-public class Main {
-    public static void main(String[] args) throws IOException {
-    	final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103";
-    	final String LOGIN_FORM_URL = "https://github.com/login";
-    	final String LOGIN_ACTION_URL = "https://github.com/session";
-    	final String USERNAME = "";
-    	final String PASSWORD = "";
+package sample.jsoup;
 
-    	// # Go to login page and grab cookies sent by server
-    	Connection.Response loginForm = Jsoup.connect(LOGIN_FORM_URL)
-    	                                     .method(Connection.Method.GET)
-    	                                     .userAgent(USER_AGENT)
-    	                                     .execute();
-    	Document loginDoc = loginForm.parse(); // this is the document containing response html
-    	HashMap<String, String> cookies = new HashMap<>(loginForm.cookies()); // save the cookies to be passed on to next request
+import java.io.FileWriter;
+import java.io.IOException;
 
-    	// # Prepare login credentials
-    	String authToken = loginDoc.select("#login > form > div:nth-child(1) > input[type=\"hidden\"]:nth-child(2)")
-    	                           .first()
-    	                           .attr("value");
+import org.jsoup.Connection.Method;
+import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
-    	HashMap<String, String> formData = new HashMap<>();
-    	formData.put("commit", "Sign in");
-    	formData.put("utf8", "e2 9c 93");
-    	formData.put("login", USERNAME);
-    	formData.put("password", PASSWORD);
-    	formData.put("authenticity_token", authToken);
-
-    	// # Now send the form for login
-    	Connection.Response homePage = Jsoup.connect(LOGIN_ACTION_URL)
-    	     .cookies(cookies)
-    	     .data(formData)
-    	     .method(Connection.Method.POST)
-    	     .userAgent(USER_AGENT)
-    	     .execute();
-
-    	System.out.println(homePage.parse().html());
-
-
-    	    /*Elements courses = document.select(".top-bootcamp-courses div.block-content h3");
-    	    for (Element course : courses) {
-    	        System.out.println(course.text());
-    	    }*/
-    }
-}
-
-
-
-
-    	Response res1 = Jsoup.connect("https://www.k.kyoto-u.ac.jp/student/la/top").userAgent(ua).timeout(500).execute();
-
-    	Document doc = res1.parse();
-    	//System.out.println(doc);
-
-    	//ログインPOST実行
-    	Response res2 = Jsoup.connect("https://www.k.kyoto-u.ac.jp/student/la/top").userAgent(ua).timeout(500)
-    	.cookies(res1.cookies())
-    	.data("KULASIS_account", "ecs-id")
-    	.data("kulasis_password", "password")
-    	.method(Method.POST)
-    	.execute();
-
-    	Document document = Jsoup.connect("https://www.k.kyoto-u.ac.jp/student/la/top").get();
-        Elements elements = document.select(".top-bootcamp-courses div.block-content h3");
-        for (Element element : elements) {
-        	String href = element.attr("href");
-        	System.out.println(href);
-        }
-
-
-    	//***** start_yearの取得 *****
-    	elements = doc.getElementsByAttributeValue("name","start_year");
-    	String startYear = new String();
-    		for (Element element : elements) {
-    		//System.out.println(element.outerHtml());
-    		startYear = element.outerHtml();
-    		}
-    	//正規表現で「value=\"....\" selected」を検索
-    	startYear = getPatternMatchFind(startYear,"value=\"....\" selected");
-    		//valueの抽出（前後の切り落とし）
-    	startYear = getSplitFrontBack(startYear, "value=\"", "\" selected");
-    		//post形式に整形
-    	startYear = "start_year=" + startYear;
-    		System.out.println(startYear);
-
-    		//***** start_monthの取得 *****
-    		elements = doc.getElementsByAttributeValue("name","start_month");
-    		String startMonth = new String();
-    		for (Element element : elements) {
-    			//System.out.println(element.outerHtml());
-    			startMonth = element.outerHtml();
-    		}
-    		//正規表現で「value=\"..\" selected」を検索
-    		startMonth = getPatternMatchFind(startMonth,"value=\"..\" selected");
-    		//valueの抽出（前後の切り落とし）
-    		startMonth = getSplitFrontBack(startMonth, "value=\"", "\" selected");
-    		//post形式に整形
-    		startMonth = "start_month=" + startMonth;
-    		System.out.println(startMonth);
-
-    }
-}
 
 public class Main {
 
-public static void main(String[] args) throws IOException {
-    Document document = Jsoup.connect("https://headlines.yahoo.co.jp/rss/list").get();
-    Elements elements = document.select(".ymuiTitle  > .btn > a");
-    for (Element element : elements) {
-    	String href = element.attr("href");
-    	System.out.println(href);
-    }
-}
-}
-public class Main {
-public static void main(String[] args) throws IOException {
-    Document document = Jsoup.connect("https://techacademy.jp/").get();
-    Elements courses = document.select(".top-bootcamp-courses div.block-content h3");
-    for (Element course : courses) {
-        System.out.println(course.text());
-    }
+	public static void main(String[] args) throws IOException {
 
-}
+		final String ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100";
+    	final String  base_url = "https://www.k.kyoto-u.ac.jp";
+    	final String login_url = base_url + "/student/la/top";
+    	final String main_url = base_url + "/student/la";
+    	//ユーザーエージェント、各種URLを指定
+
+    	String username = "username";
+    	String password = "password";
+    	//id, passwordを指定
+
+        Response res1 = Jsoup.connect(login_url)
+                .userAgent(ua)
+                .timeout(0)
+                .execute();
+     // ログイン画面取得
+
+        Document doc1 = res1.parse();
+        //System.out.println(doc1);
+
+        Response res2 = Jsoup.connect(login_url)
+                .userAgent(ua)
+                .timeout(0)
+                .cookies(res1.cookies())
+                .data("j_username", username)
+                .data("j_password", password)
+                .data("_eventId_proceed", "ログイン")
+                .followRedirects(false)
+                .userAgent(ua)
+                .method(Method.POST)
+                .execute();
+        Document doc2 = res2.parse();
+        //System.out.println(doc2);
+      //ログインPOST実行
+
+
+		FileWriter fw = null;
+		//ファイルに書き込む変数の宣言と初期化
+
+		try {
+			fw = new FileWriter("C:\\Users\\yuki morimoto\\Documents", true);
+            //ファイルに書き込む処理
+
+            //Document document = Jsoup.connect(main_url).userAgent(ua).get();
+            //HTMLの読み込み
+            Response res3 = Jsoup.connect(main_url)
+                    .userAgent(ua)
+                    .timeout(0)
+                    .cookies(res2.cookies())
+                    .method(Method.GET)
+                    .execute();
+            Document document = res3.parse();
+
+
+            for (Element e: document.select("div")) {
+                String name = e.text();
+                String link = e.attr("abs:href");
+                fw.write("{\"Name\":\"" + name + "\",\"Link\":\""+ link + "\"}\n");
+                System.out.println(name);
+                System.out.println(link);
+                System.out.println();
+            }
+
+            fw.flush();
+            //各行をファイルに書き出し
+
+		}catch(IOException e) {
+
+			System.out.println("error");
+			//例外処理
+
+		}finally {
+
+  			if(fw != null) {
+  					try {
+  						if(fw != null) {
+  							fw.close();
+  						}
+  			//ファイルを閉じる
+
+  					}catch(IOException e2) {}
+  			}
+		}
+	}
 }
