a55e1b7cdca94e61c4c0134fd3cefd9d141882fd
mrmt <44957177+mrmt1204@users.noreply.github.com>
2019-04-22 11:20:02 +0900

    Fix login_url

--- main.java
+++ main.java
@@ -15,86 +15,83 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 
 		final String ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100";
-    	final String  base_url = "https://www.k.kyoto-u.ac.jp";
-    	final String login_url = base_url + "/student/la/top";
-    	final String main_url = base_url + "/student/la";
-    	//ユーザーエージェント、各種URLを指定
+		final String  base_url = "https://www.k.kyoto-u.ac.jp";
+		final String login_url = base_url + "/student/la/top";
+		final String main_url = base_url + "/student/la";
+    //ユーザーエージェント、各種URLを指定
 
-    	String username = "username";
-    	String password = "password";
-    	//id, passwordを指定
+    String username = "username";
+    String password = "password";
+  	//id, passwordを指定
 
-        Response res1 = Jsoup.connect(login_url)
-                .userAgent(ua)
-                .timeout(0)
-                .execute();
-     // ログイン画面取得
+		Response res1 = Jsoup.connect(login_url)
+							.userAgent(ua)
+              .timeout(0)
+             	.execute();
 
-        Document doc1 = res1.parse();
-        //System.out.println(doc1);
+		Document doc1 = res1.parse();
+		// ログイン画面取得
 
-        Response res2 = Jsoup.connect(login_url)
-                .userAgent(ua)
-                .timeout(0)
-                .cookies(res1.cookies())
-                .data("j_username", username)
-                .data("j_password", password)
-                .data("_eventId_proceed", "ログイン")
-                .followRedirects(false)
-                .userAgent(ua)
-                .method(Method.POST)
-                .execute();
-        Document doc2 = res2.parse();
-        //System.out.println(doc2);
-      //ログインPOST実行
+		String action=form.get(0).attr("action");
+
+		Response res2 = Jsoup.connect("https://authidp1.iimc.kyoto-u.ac.jp"+action)
+              .userAgent(ua)
+              .timeout(0)
+              .cookies(res1.cookies())
+              .data("j_username", username)
+              .data("j_password", password)
+              .data("_eventId_proceed", "ログイン")
+              .followRedirects(false)
+              .userAgent(ua)
+							.method(Method.POST)
+              .execute();
+		Document doc2 = res2.parse();
+    //ログインPOST実行
 
 
 		FileWriter fw = null;
 		//ファイルに書き込む変数の宣言と初期化
 
 		try {
-			fw = new FileWriter("Path", true);
-            //ファイルに書き込む処理
+				fw = new FileWriter("Path", true);
+        //ファイルに書き込む処理
 
-            //Document document = Jsoup.connect(main_url).userAgent(ua).get();
-            //HTMLの読み込み
-            Response res3 = Jsoup.connect(main_url)
-                    .userAgent(ua)
-                    .timeout(0)
-                    .cookies(res2.cookies())
-                    .method(Method.GET)
-                    .execute();
-            Document document = res3.parse();
+        Response res3 = Jsoup.connect(main_url)
+                 .userAgent(ua)
+                 .timeout(0)
+                 .cookies(res2.cookies())
+                 .method(Method.GET)
+                 .execute();
+        Document document = res3.parse();
+				//coocieを使って読み込み
 
+        for (Element e: document.select("td a")) {
+            String name = e.text();
+            String link = e.attr("abs:href");
+            fw.write("{\"Name\":\"" + name + "\",\"Link\":\""+ link + "\"}\n");
+            System.out.println(name);
+            System.out.println(link);
+            System.out.println();
+        }
+				//名前とリンクを出力
 
-            for (Element e: document.select("div")) {
-                String name = e.text();
-                String link = e.attr("abs:href");
-                fw.write("{\"Name\":\"" + name + "\",\"Link\":\""+ link + "\"}\n");
-                System.out.println(name);
-                System.out.println(link);
-                System.out.println();
-            }
+        fw.flush();
+        //各行をファイルに書き出し
 
-            fw.flush();
-            //各行をファイルに書き出し
+				}catch(IOException e) {
 
-		}catch(IOException e) {
+			  System.out.println("error");
+			  //例外処理
 
-			System.out.println("error");
-			//例外処理
+				}finally {
 
-		}finally {
-
-  			if(fw != null) {
-  					try {
-  						if(fw != null) {
-  							fw.close();
-  						}
-  			//ファイルを閉じる
-
-  					}catch(IOException e2) {}
-  			}
-		}
-	}
+  					if(fw != null) {
+  							try {
+  								if(fw != null) {
+  									fw.close();
+  								}
+  									}catch(IOException e2) {}
+  					}
+				}
+	  }
 }
